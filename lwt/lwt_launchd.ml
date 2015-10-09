9d3b2bbf6553f75a23ff2ccedb22db855104837e
David Scott <dave.scott@unikernel.com>
2015-10-09 21:46:50 +0100

    Rewrite the lwt stub using an internal thread
    Using lwt.preemptive requires downstream users to compile with `-thread`
    which seems egregious. Unfortunately the lwt job interface headers are
    not guaranteed to be installed anywhere in particular (eg somewhere
    under .opam, not on the include path).
    
    Instead we fork a background pthread in stub, and return a pipe fd to
    OCaml. We marshal the results over the pipe and receive them in OCaml
    code.
    
    Signed-off-by: David Scott <dave.scott@unikernel.com>

--- lwt/lwt_launchd.ml
+++ lwt/lwt_launchd.ml
@@ -14,13 +14,32 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
  *)
-open Launchd
 open Lwt
 
+external stub_launch_activate_socket: string -> Unix.file_descr = "stub_launch_activate_socket"
+
+let read_int fd =
+  let buffer = Cstruct.create 4 in
+  Lwt_cstruct.complete (Lwt_cstruct.read fd) buffer
+  >>= fun () ->
+  return (Int32.to_int (Cstruct.BE.get_uint32 buffer 0))
+
 let activate_socket name =
-  Lwt_preemptive.detach activate_socket name
+  let fd = Lwt_unix.of_unix_file_descr (stub_launch_activate_socket name) in
+  read_int fd
   >>= function
-  | Result.Ok unix_fds ->
-    return (Result.Ok (List.map Lwt_unix.of_unix_file_descr unix_fds))
-  | Result.Error x ->
-    return (Result.Error x)
+  | 1 -> return (Result.Error (`Enoent name))
+  | 2 -> return (Result.Error `Esrch)
+  | 3 -> return (Result.Error `Ealready)
+  | 0 ->
+    read_int fd
+    >>= fun n ->
+    let rec read acc = function
+      | 0 -> return (Result.Ok acc)
+      | n ->
+        read_int fd
+        >>= fun x ->
+        let (x': Unix.file_descr) = Obj.magic x in
+        read (Lwt_unix.of_unix_file_descr x' :: acc) (n - 1) in
+    read [] n
+  | x -> failwith (Printf.sprintf "Internal error: received status code %d" x)
